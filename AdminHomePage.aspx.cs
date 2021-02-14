f36559693cc0d3705399cae3511e670a2a8a7ace
kmccollum <kmccollum@LAP-17450>
2021-02-13 19:21:16 -0500

    Revert "help me"
    This reverts commit 713fa478aea7a47168cf89c66c4f7c6b67e166fa.

--- AdminHomePage.aspx.cs
+++ AdminHomePage.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-/* turd */
+
 namespace WebFormPractice
 {
     public partial class Manager : System.Web.UI.Page
