a543c0e3f8a8ffc128e907ae62b14da69ba246a0
jason.miller66@yahoo.com <jason.miller66@yahoo.com>
2021-02-16 15:32:22 -0500

    added some stuff

--- DonorRegistrationPage.aspx.cs
+++ DonorRegistrationPage.aspx.cs
@@ -6,27 +6,39 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Diagnostics;
 
 namespace WebFormPractice
 {
     public partial class DonorRegistrationPage : System.Web.UI.Page
     {
         bool validCoordinates = true;
+        string lat;
+        string longit;
         protected void Page_Load(object sender, EventArgs e)
         {
-              
+            Debug.WriteLine("page load donor registration");
+
             if (IsPostBack)
             {
-               
-                string lat = TextBoxLatitude.Text;
-                string lon = TextBoxLongitude.Text;
+                System.Diagnostics.Debug.WriteLine("postback code");
+
+                lat = TextBoxLatitude.Text;
+                 longit = TextBoxLongitude.Text;
                 double latEntered = 0 ;
                 double longEntered = 0;
-                if (Double.TryParse(lat, out latEntered) && Double.TryParse(lon, out longEntered) )
+                if (Double.TryParse(lat, out latEntered) && Double.TryParse(longit, out longEntered) )
                 {
+                    Debug.WriteLine("inside parse double if");
+                    Debug.WriteLine("latEntered= " + latEntered);
+                    Debug.WriteLine("longEntered= " + longEntered);
                     validCoordinates = true; 
                 }
-                else { validCoordinates = false; }
+                else
+                {
+                    Debug.WriteLine("inside coordinates is false else statement");
+                    validCoordinates = false; 
+                }
 
                     SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
                 conn.Open();
@@ -36,83 +48,99 @@ namespace WebFormPractice
                 int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
                 //if the email exists
                 if (temp == 1)
-                {
+                {   
                     LabelEmailError.Visible = true;
                     LabelEmailError.Text = "Email already exists!";
+                    
 
                 }
-                else if (latEntered < -90 || latEntered > 90)
+
+                if (latEntered < -90 || latEntered > 90)
                 {
                     LabelLatitude.Text = "Latitude Must be between -90 and 90";
+
                 }
                 else if (longEntered < -180 || longEntered > 180)
                 {
                     LabelLongitude.Text = "Longitude Must be between -180 and 180";
+
                 }
                 else if (TextBoxLatitude.Text == "" || TextBoxLongitude.Text == "")
                 {
                     LabelLatitude.Text = "Enter a valid lat or long!";
+
                 }
                 else if (validCoordinates == false)
                 {
                     LabelLatitude.Text = "You did not enter a valid number!";
-                    conn.Close();
+
                 }
                 else
                 {
-                    conn.Close();
                     Response.Redirect("Default.aspx");
                 }
-
-               
-
+                Response.Redirect("Default.aspx");
 
 
 
-            }
+
+
+
+
+
+
+
+                conn.Close();
+
+
+            }//end is postback
         }
     
 
         protected void ButtonSubmit_Click(object sender, EventArgs e)
         {
 
+            System.Diagnostics.Debug.WriteLine("submit button code");
+
 
             try
-            {   
-                SqlConnection conn2 = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
-                conn2.Open();
-                //need to adjust for recipient and not donor
-                string insertQuery = "insert into Donor (Email,Password,phone, zip) values ( @email, @password,@phone, @zip)";
-                SqlCommand com = new SqlCommand(insertQuery, conn2);
+            {
+                System.Diagnostics.Debug.WriteLine("inside database try statement");
+                string city = TextBoxCity.Text;
+                string longitude = TextBoxLongitude.Text;
+                string latitude = TextBoxLongitude.Text;
 
 
-                com.Parameters.AddWithValue("@email", TextBoxEmail.Text);
-                com.Parameters.AddWithValue("@password", TextBoxPass.Text);
-                com.Parameters.AddWithValue("@zip", TextBoxZip.Text);
-                com.Parameters.AddWithValue("@phone", TextBoxPhone.Text);
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+                conn.Open();
+                string insertQuery = "insert into LOCATIONS(LOC_NAME, LATITUDE, LONGITUDE) values(@loc_name, @latitude, @longitude)";
 
+                    SqlCommand cmd = new SqlCommand( insertQuery, conn);
+                    cmd.Parameters.AddWithValue("@loc_name", city);
+                    cmd.Parameters.AddWithValue("@latitude", lat);
+                    cmd.Parameters.AddWithValue("@longitude", longit);
+                  
+                    cmd.ExecuteNonQuery();
+                    conn.Close(); 
 
-                com.ExecuteNonQuery();
+                
 
-
-                SqlCommand com2;
-                string locationQuery = "insert into LOCATIONS (LOC_NAME, LATITUDE, LONGITUDE)" +
-                    "values (@loc_name, @latitude, longitude)";
-                com2 = new SqlCommand(locationQuery, conn2);
-                com2.Parameters.AddWithValue("@loc_name", TextBoxCity.Text);
-                com2.Parameters.AddWithValue("@latitude", TextBoxLatitude.Text);
-                com2.Parameters.AddWithValue("@longitude", TextBoxLongitude);
-
-                com.ExecuteNonQuery();
+             
 
 
 
-               
+
+
+
+
             }
             catch (Exception ex)
             {
                 Response.Write("Error from catch:" + ex.ToString());
             }
+           
+
+
 
 
 
