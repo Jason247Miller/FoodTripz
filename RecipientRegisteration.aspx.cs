8053764809d2e0e0a5cd99113fc324c246c42f91
Tyler <34220819+tylerig@users.noreply.github.com>
2021-02-17 01:45:35 -0500

    The Recipient Registration page works and adds to the database. I did some research on the "false" falgs I added after the redirects, we NEED to flag them flase or else it throws an exception and fials to run any code.

--- RecipientRegisteration.aspx.cs
+++ RecipientRegisteration.aspx.cs
@@ -7,85 +7,88 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Configuration;
 using System.Diagnostics;
+using System.Data;
 
 namespace WebFormPractice
 {
     public partial class RecipientRegistration : System.Web.UI.Page
     {
-       
+
         protected void Page_Load(object sender, EventArgs e)
         {
             if (IsPostBack)
             {
 
 
-              
-                    SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
-                    conn.Open();
 
-                    string checkuser = "select count(*) from Recipient where Email='" + TextBoxEmail.Text + "'";
-                    SqlCommand com = new SqlCommand(checkuser, conn);
-                    int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
+                SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+                conn.Open();
+
+                string checkuser = "select count(*) from Recipient where Email='" + TextBoxEmail.Text + "'";
+                SqlCommand com = new SqlCommand(checkuser, conn);
+                int temp = Convert.ToInt32(com.ExecuteScalar().ToString());
                 //if the email exists
-                if (temp >=1)
+                if (temp >= 1)
                 {
-                    LabelEmailError.Visible = true; 
+                    LabelEmailError.Visible = true;
                     LabelEmailError.Text = "Email already exists!";
                     conn.Close();
                 }
                 else
                 {
-                    Response.Redirect("Login.aspx");
+                    Response.Redirect("Login.aspx", false);
                 }
-                   
-                
-                
+
+
+
 
             }
         }
 
-
-
         //Button1 is Submit Button
+        //code below adds to db
         protected void Button1_Click(object sender, EventArgs e)
         {
             Debug.WriteLine("inside button click");
 
-                try
-                {   //Everytime button is clicked, a new unique id will be generated
 
-                    SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
-                    conn.Open();
-                    //need to adjust for recipient and not donor
-                    string insertQuery = "insert into Recipient (FIRST_NAME,LAST_NAME,EMAIL,PHONE_NUM,PASSWORD,ZIP) values (@fname,@lname, @email,@phone, @password,@zip)";
-                    SqlCommand com = new SqlCommand(insertQuery, conn);
-
-                   
-                    com.Parameters.AddWithValue("@email", TextBoxEmail.Text);
-                    com.Parameters.AddWithValue("@password", TextBoxPass.Text);
-                    com.Parameters.AddWithValue("@lname", TextBoxLname.Text);
-                    com.Parameters.AddWithValue("@fname", TextBoxFname.Text);
-                    com.Parameters.AddWithValue("@zip", TextBoxZip.Text);
-                    com.Parameters.AddWithValue("@phone", TextBoxPhone.Text);
-
-
-                com.ExecuteNonQuery();
-                   
-                   
-                    //Response.Write("Registration was successful");
-
-                    conn.Close();
-                }
-                catch (Exception ex)
+            string _connStr = ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString;
+            string _query = "INSERT INTO RECIPIENT(FIRST_NAME,LAST_NAME,EMAIL,PHONE_NUM,PASSWORD,ZIP) values(@fname,@lname, @email,@phone,@password,@zip)";
+            using (SqlConnection conn = new SqlConnection(_connStr))
+            {
+                using (SqlCommand comm = new SqlCommand())
                 {
-                    Response.Write("Error:" + ex.ToString());
-                }
+                    comm.Connection = conn;
+                    comm.CommandType = CommandType.Text;
+                    comm.CommandText = _query;
+                    comm.Parameters.AddWithValue("@fname", TextBoxFname.Text);
+                    comm.Parameters.AddWithValue("@lname", TextBoxLname.Text);
+                    comm.Parameters.AddWithValue("@email", TextBoxEmail.Text);
+                    comm.Parameters.AddWithValue("@phone", TextBoxPhone.Text);
+                    comm.Parameters.AddWithValue("@password", TextBoxPass.Text);
+                    comm.Parameters.AddWithValue("@zip", TextBoxZip.Text);
+                    try
+                    {
+                        conn.Open();
+                        comm.ExecuteNonQuery();
+                    }
 
-            
-            
-            
-           
-           
+                    catch (Exception ex)
+                    {
+                        Response.Write("Error:" + ex.ToString());
+                    }
+                }
+            }
+
         }
     }
 }
+
+
+            
+            
+            
+           
+           
+        
+    
